172346229ae59246201883c2f8d4299ea6a1ac27
andrestoroa <andrestoro5@hotmail.com>
2023-10-17 22:29:40 -0500

    Inicio con la creacion de la barra de navegacion y del banner

---
+++ proyectomiercoles/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './estilos.css';
+import Barranava from './barranavegacion';
+import Banner from './banner';
+
+function App() {
+  return (
+    <div>
+        <Barranava/>
+        <Banner/>
+    </div>
+  );
+}
+
+export default App;
